2e9c4175afbf96f4bff587fc1f53ed6cf9d8712c
Nick Joyce <brawlyrox@gmail.com>
2018-10-14 15:56:11 -0400

    Commented out boilerplate code for async/await

--- src/requests.js
+++ src/requests.js
@@ -9,31 +9,33 @@ const getPuzzle = async (wordCount) => {
     }
 }
 
-const getCountry = async (countryCode) => {
-    const response = await fetch('//restcountries.eu/rest/v2/all')
-        if (response.status === 200) {
-            const data = await response.json()
-            const country = data.find(country => country.alpha2Code === countryCode)
-            return country.name
-        } else {
-            throw new Error('Unable to fetch countries') 
-        }
-    }
-      
-const getLocation = async () => {
-    const response = await fetch('//ipinfo.io/json?token=be4b4d203c7054')
-        if (response.status === 200) {
-            return await response.json()
-            
-        } else {
-            throw new Error('Unable to get the current location')
-        }
-    }
+// Code using async/await - keeping to use as a reference
 
-const getCurrentCountry = async () => {
-    const location = await getLocation()
-    const country = await getCountry(location.country)
-    return country
-}
+// const getCountry = async (countryCode) => {
+//     const response = await fetch('//restcountries.eu/rest/v2/all')
+//         if (response.status === 200) {
+//             const data = await response.json()
+//             const country = data.find(country => country.alpha2Code === countryCode)
+//             return country.name
+//         } else {
+//             throw new Error('Unable to fetch countries') 
+//         }
+//     }
+      
+// const getLocation = async () => {
+//     const response = await fetch('//ipinfo.io/json?token=be4b4d203c7054')
+//         if (response.status === 200) {
+//             return await response.json()
+            
+//         } else {
+//             throw new Error('Unable to get the current location')
+//         }
+//     }
+
+// const getCurrentCountry = async () => {
+//     const location = await getLocation()
+//     const country = await getCountry(location.country)
+//     return country
+// }
 
 export { getPuzzle as default }
